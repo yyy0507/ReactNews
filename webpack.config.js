15a3236383c68ffa2fa4b307ad5ecbe057777086
yyy0507 <2313367541@qq.com>
2017-11-29 11:26:06 +0800

    页头页脚开发

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	context: __dirname + '/src',
+	entry: './js/root.js',
+	module: {
+		loaders: [
+		{
+			test: /\.js?$/,
+			exclude: /(node-modules)/,
+			loader: 'babel-loader',
+			query: {
+				presets: ['react', 'es2015'],
+				plugins: ['react-html-attrs'],//添加组件的插件配置
+			}
+		},
+		//下面是添加的css的loader也就是css模块化的配置方法
+		{
+			test: /\.css$/,
+			//loader: "style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]"
+			loader: 'style-loader!css-loader'
+		},
+		]
+	},
+	output: {
+		path: __dirname + '/src/',
+		filename: "bundle.js"
+	}
+};
